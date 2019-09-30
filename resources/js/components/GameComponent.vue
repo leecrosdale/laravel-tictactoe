792d14532e37bcdb5e09d1fe3919c9a9eba431bf
Lee Crosdale <lee.crosdale@enovate.co.uk>
2019-09-30 13:04:07 +0100

    INIT

---
+++ resources/js/components/GameComponent.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        <canvas width="600" height="600" style="background: #000;" ref="game-canvas"></canvas>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                // By creating the provider in the data property, it becomes reactive,
+                // so child components will update when `context` changes.
+                provider: {
+                    // This is the CanvasRenderingContext that children will draw to.
+                    context: null,
+                    tileW: 200,
+                    tileH: 200,
+                    tileCount: 3,
+                },
+                renderingStatus: 'Not Rendering'
+            }
+        },
+        mounted() {
+            this.provider.context = this.$refs['game-canvas'].getContext('2d');
+            this.render()
+        },
+        methods: {
+            render() {
+
+                if (!this.provider.context) return;
+                const ctx = this.provider.context;
+
+
+                for (let y = 0; y<this.provider.tileCount; y++)
+                {
+                    for (let x = 0; x<this.provider.tileCount; x++)
+                    {
+                        ctx.beginPath();
+                        ctx.rect(
+                            x * this.provider.tileW,
+                            y * this.provider.tileH,
+                            this.provider.tileW,
+                            this.provider.tileH
+                        );
+                        ctx.fillStyle = 'white';
+                        ctx.fill();
+                        ctx.lineWidth = 2;
+                        ctx.strokeStyle = '#003300';
+                        ctx.stroke();
+                    }
+                }
+            }
+        }
+    }
+</script>
